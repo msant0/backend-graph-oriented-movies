a899dbfe21fba61cb1395ae531ee5b6b1ca478be
Mateus Santos <mateusd.santos10@gmail.com>
2020-08-06 06:50:21 -0300

    add movies, actors and relationships

---
+++ app.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const { ppid, title } = require('process')
+const neo4j = require('neo4j-driver')
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'neo4j'))
+const session = driver.session()
+
+app.get('/', function (req, res) {
+    session
+        .run('MATCH(n: Movie) RETURN n')
+        .then(function (result) {
+            const movieArr = []
+            result.records.forEach(function (record) {
+                movieArr.push({
+                    id: record._fields[0].identity.low,
+                    title: record._fields[0].properties.title,
+                    year: record._fields[0].properties.year
+                })
+            })
+
+            session
+                .run('MATCH(n: Person) RETURN n')
+                .then(function (result) {
+                    const personArr = []
+                    result.records.forEach(function (record) {
+                        personArr.push({
+                            id: record._fields[0].identity.low,
+                            name: record._fields[0].properties.name,
+                            born: record._fields[0].properties.born
+                        })
+                    })
+                    res.render('index', {
+                        movies: movieArr,
+                        persons: personArr
+                    })
+                })
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
+})
+
+app.post('/movie/add', function (req, res) {
+    const title = req.body.title
+    const year = req.body.year
+
+    session
+        .run(`CREATE(n:Movie { title: '${title}', year: '${year}'}) RETURN n.title`)
+        .then(function (result) {
+            res.redirect('/')
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
+})
+
+app.post('/person/add', function (req,res){
+    const name = req.body.name
+    const born = req.body.born
+
+    session
+        .run(`CREATE(n:Person { name: '${name}', born: '${born}'}) RETURN n.name`)
+        .then(function(result){
+            res.redirect('/')
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+})
+
+app.post('/movie/person/add', function (req,res){
+    const title = req.body.title
+    const name = req.body.name
+
+    session
+        .run(`MATCH(p: Person { name: '${name}'}), (m: Movie {title: '${title}'}) MERGE(p)-[r:ACTED_IN]-(m) RETURN p,m`)
+        .then(function(result){
+            res.redirect('/')
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+})
+
+app.listen(3000)
+
+console.log('Server started on Port 3000')
+
+module.exports = app
